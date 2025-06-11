6122229326a3d790e16c17b79e45ba6d2b093bff
CarlosX-GitH <carlosxaviersosa24@gmail.com>
2025-06-11 18:59:17 -0400

    Fixes

---
+++ Practicas en PHP/library-sys/backend/pages/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Hello from Inertia + Vue!</h1>
+</template>
---
+++ Practicas en PHP/library-sys/backend/resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel Inertia Vue</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    @inertia
+</body>
+</html>
